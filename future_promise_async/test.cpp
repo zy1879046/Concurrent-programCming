33705fe3ba8daae230e0f31b970e14b91a01d7b2
xiaojiu <3024932397@qq.com>
2024-04-22 22:05:26 +0800

    test

--- future_promise_async/test.cpp
+++ future_promise_async/test.cpp
@@ -1,12 +1,16 @@
 #include "include/ThrealPool.h"
 int main()
 {
-    int m = 0;
-    std::future<int> fut = ThreadPool::instance().commit([](int& m){
-        m = 1024;
-        std::cout<<"inner set is: "<<m <<std::endl;
-        std::cout<<"m address is: "<<&m<<std::endl;
-        return 34;
-    },std::ref(m));
-    std::cout<<fut.get()<<std::endl;
+    for(int i = 0; i < 100; ++i)
+    {
+        int m = 0;
+        std::future<int> fut = ThreadPool::instance().commit([](int& m){
+            m = 1024;
+            std::cout<<"inner set is: "<<m <<std::endl;
+            std::cout<<"m address is: "<<&m<<std::endl;
+            return 34;
+        },std::ref(m));
+        std::cout<<fut.get()<<std::endl;
+    }
+    return 0;
 }
